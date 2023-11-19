1b4f5accb69236764048cf09f833d8cef2551c61
0xpure <arseniygol2017@gmail.com>
2023-11-19 16:30:18 +0200

    ver 2.1a

--- src/components/Projects/ProjectsInfo.js
+++ src/components/Projects/ProjectsInfo.js
@@ -1,9 +1,8 @@
 import pr1 from "./pr1.png";
-import pr2 from "./pr2.png"
-import pr3 from "./pr3.png"
-import pr4 from "./pr4.png"
-import pr5 from "./pr5.png"
-
+import pr2 from "./pr2.png";
+import pr3 from "./pr3.png";
+import pr4 from "./pr4.png";
+import pr5 from "./pr5.png";
 
 const ProjectList = {
   pr1: [
@@ -21,8 +20,20 @@ const ProjectList = {
     "If you're on the site now, it's probably working",
     2,
   ],
-  p3: ["PURESERVER (Minecraft)", "?", pr3, "in develop", 3],
-  pr4: ["Telegram bot", "?", pr4, "in develop", 4],
+  p3: [
+    "PURESERVER (Minecraft)",
+    "https://github.com/0xpure/pureserver-plugin-mine",
+    pr3,
+    "in develop",
+    3,
+  ],
+  pr4: [
+    "Telegram bot",
+    "https://github.com/0xpure/tgpythonbot",
+    pr4,
+    "in develop",
+    4,
+  ],
   pr5: [
     "Palomaresbooks.ru",
     "https://github.com/0xpure/palomaresbooks/tree/master",
